7c2a76ef9b0ba1a1afc01b51ed921b7a644c4639
MARIANNACROCHA <marianna.camillo@gmail.com>
2020-08-18 05:19:01 -0300

    mudança do alert

--- script/index.js
+++ script/index.js
@@ -1,47 +1,60 @@
 $( document ).ready(function() {
 
-    $("#enviarbtn").click(function(){
+    $("#enviarbtn").click(function(x){
         let month = $("#month").val();
         let day = $("#day").val();
 
         if ((month == 1 && day >=20) ||(month == 2 && day <=18))  {
-            alert("Aquario");
+        $("#resultado").html("Aquário");    
+        // alert("Aquario");
         } 
         else if ((month == 2 && day >=19) ||(month == 3 && day <=20))  {
-            alert("Peixes");
+            $("#resultado").html("Peixes");
+            // alert("Peixes");
         }
         else if ((month == 3 && day >=21) ||(month == 4 && day <=19))  {
-            alert("Áries");
+            $("#resultado").html("Áries");
+            // alert("Áries");
         }
         else if ((month == 4 && day >=20) ||(month == 5 && day <=20))  {
-            alert("Touro");
+            $("#resultado").html("Touro");
+            // alert("Touro");
         }
         else if ((month == 5 && day >=21) ||(month == 6 && day <=21))  {
-            alert("Gêmeos");
+            $("#resultado").html("Gêmeos");
+            // alert("Gêmeos");
         }
         else if ((month == 6 && day >=22) ||(month == 7 && day <=22))  {
-            alert("Câncer");
+            $("#resultado").html("Câncer");
+            // alert("Câncer");
         }
         else if ((month == 7 && day >=23) ||(month == 8 && day <=22))  {
-            alert("Leão");
+            $("#resultado").html("Leão");
+            // alert("Leão");
         }
         else if ((month == 8 && day >=23) ||(month == 9 && day <=22))  {
-            alert("Virgem");
+            $("#resultado").html("Virgem");
+            // alert("Virgem");
         }
         else if ((month == 9 && day >=23) ||(month == 10 && day <=22))  {
-            alert("Libra");
+            $("#resultado").html("Libra");
+            // alert("Libra");
         }
         else if ((month == 10 && day >=23) ||(month == 11 && day <=21))  {
-            alert("Escorpião");
+            $("#resultado").html("Escorpião");
+            // alert("Escorpião");
         }
         else if ((month == 11 && day >=22) ||(month == 12 && day <=21))  {
-            alert("Sagitário");
+            $("#resultado").html("Sagitário");
+            // alert("Sagitário");
         }
         else if ((month == 12 && day >=22) ||(month == 1 && day <=19))  {
-            alert("Capricórnio");
+            $("#resultado").html("Capricórnio");
+            // alert("Capricórnio");
         }
         else {
-            alert("Data digitada incorretamente")
+            $("#resultado").html("Data digitada incorretamente");
+            // alert("Data digitada incorretamente")
         }
     });
 })
